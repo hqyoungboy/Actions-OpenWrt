9cf943a2e056aeeece1f9ae4c6b9384ba92bd68f
hqyoungboy <119115158+hqyoungboy@users.noreply.github.com>
2023-02-28 10:04:35 +0800

    Update diy-part2.sh

--- diy-part2.sh
+++ diy-part2.sh
@@ -25,5 +25,5 @@ rm -rf feeds/app/luci-app-adguardhome
 git clone https://github.com/rufengsuixing/luci-app-adguardhome.git Package/luci-app-adguardhome
 
 #pushd package/lean
-#git clone --depth=1 https://github.com/rufengsuixing/luci-app-adguardhome
+#git clone --depth=1 https://github.com/hqjbboy/small-package/tree/main/diy/.packages/luci-app-adguardhome
 #popd
